cf70b36435be538f6e98a0ed10a9a3f6de575f24
CJS@Acer <c.junsheng@hotmail.com>
2020-07-04 23:42:30 +0800

    Remove conflicts

--- server/notion-py-api/singleTimeTask.py
+++
@@ -1,9 +0,0 @@
-from NotionWrapper import Detail, NotionWrapper
-
-notion = NotionWrapper("./config.json")
-detail_collection = notion.get_table("detail_list")
-
-for row in detail_collection.get_rows():
-    row.poc = "[{} ({})](https://wa.me/65{})".format(row.poc,
-                                                     row.poc_contact_no, row.poc_contact_no)
-    print(row.poc)
